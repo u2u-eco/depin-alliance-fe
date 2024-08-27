1df5020cb32bdfac0a04842e7f84629a0c0eef0a
steven-tp <steven-tp>
2024-08-27 17:30:56 +0700

    [Task] Add api upgrade

--- src/app/services/devices.ts
+++ src/app/services/devices.ts
@@ -3,3 +3,7 @@ import https from '../constants/https'
 export const getUserDevice = () => {
   return https.get('/devices/user-device-item')
 }
+
+export const getDevicesByType = (type: string) => {
+  return https.get(`/devices/item?type=${type}`)
+}
