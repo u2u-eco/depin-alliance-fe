cec012e14277c0f227f56139d774d5e9af3ff30d
steven-tp <steven-tp>
2024-09-07 09:23:25 +0700

    [Task] Update rate purchase

--- src/app/shop/components/shop-item.tsx
+++ src/app/shop/components/shop-item.tsx
@@ -240,11 +240,11 @@ export default function ShopItem({ filterOptions }: IShopItem) {
                     <IconPoint className="size-5" color />
                     <span className="font-geist">
                       {totalAmount
-                        ? `${formatNumber(userInfo?.ratePurchase ? totalAmount - (totalAmount * userInfo.ratePurchase) / 100 : totalAmount, 0, 0)}`
+                        ? `${formatNumber(userInfo?.ratePurchase ? userInfo.ratePurchase * totalAmount : totalAmount, 0, 0)}`
                         : 0}
                     </span>
                   </div>
-                  {userInfo?.ratePurchase && (
+                  {userInfo?.ratePurchase && userInfo?.ratePurchase < 1 && (
                     <div className="flex items-center space-x-1 opacity-65">
                       <IconPoint className="size-4" color />
                       <span className="font-geist text-xs line-through">
