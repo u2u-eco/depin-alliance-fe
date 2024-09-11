d4ce1ba8b7a586624c0cb9c865e9378389876c30
steven-tp <steven-tp>
2024-09-11 08:32:13 +0700

    [Bug] Bug check scroll

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,16 +1,15 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 // import CustomNavbar from './custom-navbar'
 // import { usePathname } from 'next/navigation'
 import TelegramProvider from '../../contexts/telegram.context'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 export default function Layout({ children }: any) {
-  // const pathName = usePathname()
-
   const queryClient = new QueryClient()
+
   return (
     <div className="wrapper min-h-[100vh]">
       <Toaster
