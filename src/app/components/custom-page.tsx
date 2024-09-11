b624a9334a35cd181db99019cb34cac5964bed04
steven-tp <steven-tp>
2024-09-11 20:04:14 +0700

    Test scroll

--- src/app/components/custom-page.tsx
+++ src/app/components/custom-page.tsx
@@ -33,7 +33,7 @@ const CustomPage = ({ children, classNames }: Pageprops) => {
     <AnimatePresence mode="wait">
       {/* <div className={cn(classNames?.wrapper)}> */}
       <div
-        className={`${isShowSidebar ? 'h-[calc(100vh-75px)]' : 'h-full'}  overflow-y-auto flex flex-col hide-scrollbar`}
+        className={`${isShowSidebar ? 'h-[calc(100vh-75px)]' : 'h-[100vh]'}  bg-gray-850  overflow-y-auto flex flex-col hide-scrollbar`}
       >
         <div className="container-custom">
           <motion.div
