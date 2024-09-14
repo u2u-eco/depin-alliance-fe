b2d0ecb6fa9d77f78c081893dd5340153f0ad389
frankie <frankie>
2024-09-14 02:33:58 +0000

    Update script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+cd /home/ubuntu/depin-alliance-fe
 git pull origin staging
 
+NPM=/home/ubuntu/.nvm/versions/node/v18.20.4/bin/npm
+PM2=/home/ubuntu/.nvm/versions/node/v18.20.4/bin/pm2
+
 # Install dependencies
-npm install || { echo "npm install failed"; exit 1; }
+$NPM install || { echo "npm install failed"; exit 1; }
 
 # Run build
-npm run build || { echo "Build failed"; exit 1; }
+$NPM run build || { echo "Build failed"; exit 1; }
 
 
-pm2 restart depin-fe
+$PM2 restart depin-fe
