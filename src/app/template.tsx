daa0bcad582facaa374be27bdbc2afeb0b3ed3e2
steven-tp <steven-tp>
2024-09-16 15:41:35 +0700

    [Bug] Update logic auth

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -10,7 +10,6 @@ import useCommonStore from '@/stores/commonStore'
 import { userLeague } from '@/services/league'
 import Loading from './components/loading'
 import { toast } from 'sonner'
-import { CURRENT_STATUS } from '@/interfaces/i.user'
 import { useRouter } from 'next/navigation'
 export default function Template({ children }: { children: React.ReactNode }) {
   const { webApp } = useTelegram()
@@ -45,12 +44,6 @@ export default function Template({ children }: { children: React.ReactNode }) {
         setCurrentStatus({ status: res.data.currentStatus })
         await getUserInfo()
         await getUserConfig()
-        if (
-          res.data.currentStatus === CURRENT_STATUS.CLAIMED ||
-          res.data.currentStatus === CURRENT_STATUS.MINING
-        ) {
-          router.push('/home')
-        }
         _getUserLeague()
         Cookies.set(CURRENT_STATUS_STORAGE, res.data?.currentStatus)
         setToken({ token: res.data?.accessToken })
