d656438a22ea0ee2bd4e33f63ee6300513b78233
steven-tp <steven-tp>
2024-10-02 10:07:46 +0700

    [Task] Update modal

--- src/app/components/custom-modal.tsx
+++ src/app/components/custom-modal.tsx
@@ -48,7 +48,7 @@ const CustomModal = ({
       scrollBehavior={placement === 'top' ? 'outside' : 'normal'}
       hideCloseButton
       classNames={{
-        wrapper: `${placement === 'top' && maxHeight && webApp?.platform !== 'android' ? `max-h-[60vh] overflow-y-auto no-scrollbar` : 'max-h-[100vh]'}`,
+        wrapper: `${placement === 'top' && maxHeight && webApp?.platform !== 'android' ? `max-h-[60vh] overflow-y-auto no-scrollbar` : ''}`,
         base: full
           ? `max-w-full m-0 rounded-none w-full h-full p-0 bg-black/80 backdrop-blur-[4px]`
           : `max-w-full m-0 rounded-none h-full xs:h-auto bg-transparent xs:bg-green-600 before:content-[''] before:bottom-0 before:absolute before:left-0 before:size-full before:z-[-1] before:bg-[linear-gradient(to_top,#000000,#002012)] pt-10 xs:pt-12 pb-8 xs:pb-10 px-4 xs:border-t-[0.5px] min-[355px]:border-t-green-600 xs:[clip-path:_polygon(40px_0,100%_0,100%_100%,_0_100%,0_40px)] before:xs:[clip-path:_polygon(40px_0,100%_0,100%_100%,_0_100%,0_40px)] after:content-[''] after:absolute after:top-0 after:right-[40px] after:w-[60px] after:h-[160px] after:rounded-full after:rotate-[-150deg] after:bg-gradient after:blur-[68px] after:opacity-80 after:z-[-1] `,
@@ -58,7 +58,7 @@ const CustomModal = ({
       <ModalContent>
         {(onClose) => (
           <div
-            className={`max-w-[480px] mx-auto w-full ${full ? 'h-full' : 'max-xs:overflow-y-auto max-xs:no-scrollbar'}`}
+            className={`max-w-[480px] mx-auto w-full ${full ? 'h-full' : 'overflow-y-auto no-scrollbar'}`}
           >
             {full ? (
               <>{background}</>
