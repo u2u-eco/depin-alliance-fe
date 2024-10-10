47bb08c10bb3f21883ed3bcd0b713c8954e790c5
steven-tp <steven-tp>
2024-10-10 17:00:21 +0700

    [Task] Add game puzzle

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,22 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false
   },
+  async rewrites() {
+    return [
+      {
+        source: '/games/puzzle',
+        destination: '/games/puzzle/index.html'
+      },
+      {
+        source: '/games/monster',
+        destination: '/games/monster/index.html'
+      },
+      {
+        source: '/games/solve-math',
+        destination: '/games/solve-math/index.html'
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
