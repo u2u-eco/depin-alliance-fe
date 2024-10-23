fe89853326f2b081441abf0119fe1b0e72280a26
steven-tp <steven-tp>
2024-10-23 16:39:57 +0700

    [Task] Update mission done solve math

--- public/games/solve-math/js/game.js
+++ public/games/solve-math/js/game.js
@@ -932,7 +932,7 @@ function checkUserFormula() {
   if (proceedAnswer) {
     if (eval(userFormula) == eval(finalMath)) {
       //correct answer
-      if (totalSolve + 1 === 9 && !isPassMission) {
+      if (totalSolve + 1 === 5 && !isPassMission) {
         isPassMission = true
         window.parent.postMessage('WIN', '*')
         return
