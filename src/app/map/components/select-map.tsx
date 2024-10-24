87cc3031ab98607fcdc2e8789f381df34c21ac0f
hungnd1 <Hungnd1@1293969>
2024-10-24 15:31:25 +0700

    [Task] Update select map

--- src/app/map/components/select-map.tsx
+++ src/app/map/components/select-map.tsx
@@ -4,7 +4,9 @@ import {
   IconMapAntarctica,
   IconMapAsia,
   IconMapEurope,
-  IconMapOceania
+  IconMapNorthAmerica,
+  IconMapOceania,
+  IconMapSouthAmerica
 } from '@/app/components/icons'
 import { MAP_TYPE } from '@/constants'
 import { useAppSound } from '@/hooks/useAppSound'
@@ -68,15 +70,15 @@ const listMapConfig = [
     list: [
       {
         id: 'continent_4',
-        image: <IconMapAntarctica className="size-6 xs:size-7 2xs:size-8 mx-auto" />,
-        title: MAP_TYPE.ANTARCTICA,
+        image: <IconMapNorthAmerica className="size-6 xs:size-7 2xs:size-8 mx-auto" />,
+        title: MAP_TYPE.NORTH_AMERICA,
         class:
           'before:[clip-path:_polygon(50%_0,100%_var(--shape),100%_calc(100%_-_var(--shape)),calc(50%_+_var(--line)*2)_100%,0_calc(100%_-_var(--shape)_-_var(--line)),0_var(--shape));]'
       },
       {
         id: 'continent_3',
-        image: <IconMapAmerica className="size-6 xs:size-7 2xs:size-8 mx-auto" />,
-        title: MAP_TYPE.AMERICA,
+        image: <IconMapSouthAmerica className="size-6 xs:size-7 2xs:size-8 mx-auto" />,
+        title: MAP_TYPE.SOUTH_AMERICA,
         class:
           'before:[clip-path:_polygon(50%_0,100%_var(--shape),100%_calc(100%_-_var(--shape)_-_var(--line)),calc(50%_-_var(--line)*2)_100%,0_calc(100%_-_var(--shape)),0_var(--shape));]'
       }
@@ -136,7 +138,7 @@ const SelectMap = ({ activeArea }: MapProps) => {
               <div className="relative space-y-1.5 xs:space-y-2">
                 {el.image}
                 <p
-                  className={`font-airnt font-medium text-[10px] xs:text-[11px] 2xs:text-xs !leading-[14px] xs:!leading-[16px] tracking-[1px] uppercase ${activeItem === el.id ? 'text-green-900' : 'text-title text-shadow-white'}`}
+                  className={`font-airnt font-medium text-[10px] xs:text-[11px] 2xs:text-xs !leading-[14px] xs:!leading-[16px] min-h-[14px] xs:min-h-[16px] tracking-[1px] uppercase text-center ${activeItem === el.id ? 'text-green-900' : 'text-title text-shadow-white'}`}
                 >
                   {el.title}
                 </p>
